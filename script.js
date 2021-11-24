b8d989cf7ad1d876698562a911e07e828cd658fd
swells25 <89562708+swells25@users.noreply.github.com>
2021-11-23 19:46:51 -0700

    commit

---
+++ script.js
@@ -0,0 +1,44 @@
+//for(var i = 0; i < 100; i++){
+//    console.log(i)
+//}
+
+//for(var i = 0; i < 10; i++){
+//    console.log("I'm getting good ar for loops")
+//}
+
+//var favFoods = ["pizza","pasta", "ice cream", "banana"]
+//favFoods.lenght//4
+
+//console.log(favFoods[0])
+//console.log(favFoods[1])
+//console.log(favFoods[2])
+//console.log(favFoods[3])
+
+//for(var i = 0; i < favFoods.length; i++){
+//    console.log(favFoods[i])
+//}
+
+//var numbers = [1, 2, 3, 4, 5, 6]
+
+//for(var i = 0; i , numbers.length; i++){
+ //   if(numbers[i] % 2 === 0){
+ //       console.log(numbers[i])
+ //   }
+//}
+
+//var count = 0
+//while(count < 10){
+//    console.log('hi')
+//    count++
+//}
+
+
+for(var i = 0; i <= 100; i++){
+    
+    if(i % 2 === 0) {
+        console.log("Even!!!")
+    }   else {
+        console.log("Odd...")
+    }
+    
+}
